12a811ef6df756999c7c2b27c0f77b6f931cd098
Marc <marc.ho380@gmail.com>
2018-06-04 13:04:14 -0700

    Added material ui grid list to display listings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import request from 'superagent';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 class App extends Component {
     constructor(props) {
@@ -11,11 +15,40 @@ class App extends Component {
         };
     }
 
+    componentWillMount() {
+        request
+            .get('https://thisopenspace.com/lhl-test')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .query({ page: this.state.currentPage })
+            .end(function(err, res){
+                if (res.body) {
+                    this.setState({ listings: res.body });
+                }
+        }.bind(this)); 
+    }
+
     render() {
+        const allListings = this.state.listings;
+
+        let listingTile = (allListings.length === 0) ? '' :
+            <div className="grid-div">
+                <GridList cellHeight={180} className="grid-list">
+                    {allListings.data.map(tile => (
+                        <GridListTile 
+                            className="grid-list-tile"
+                            key={tile.id}>
+                            <img src={tile.primary_photo_css_url_small} alt={tile.name} />
+                            <GridListTileBar
+                                title={tile.name}
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>;
+
         return (
         <div className="App">
-            <p className="App-intro">
-            </p>
+            {listingTile}
         </div>
         );
     }
