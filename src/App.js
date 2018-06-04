b13e0b249b21f90a442b75c337e22f2871669ef7
Marc <marc.ho380@gmail.com>
2018-06-04 13:24:19 -0700

    Added styling changes and prev/next button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import request from 'superagent';
+import Button from '@material-ui/core/Button';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 class App extends Component {
@@ -29,10 +31,32 @@ class App extends Component {
 
     render() {
         const allListings = this.state.listings;
-
         let listingTile = (allListings.length === 0) ? '' :
             <div className="grid-div">
                 <GridList cellHeight={180} className="grid-list">
+                    <GridListTile key="Subheader" className="subheader" cols={2}>
+                        <ListSubheader className="subheader-title" component="div">Open Listings</ListSubheader>
+                        <div className="page-btns">
+                            <Button
+                                onClick={() => this.prevPage()}
+                                variant="raised"
+                                color="primary"
+                                className="prev-btn"
+                                disabled={this.state.currentPage == 1}
+                                >
+                                Prev
+                            </Button>
+                            <Button
+                                onClick={() => this.nextPage()}
+                                variant="raised"
+                                color="primary"
+                                className="next-btn"
+                                disabled={this.state.listings.length === 0}
+                                >
+                                Next
+                            </Button>
+                        </div>
+                    </GridListTile>
                     {allListings.data.map(tile => (
                         <GridListTile 
                             className="grid-list-tile"
@@ -44,6 +68,26 @@ class App extends Component {
                         </GridListTile>
                     ))}
                 </GridList>
+                <div className="page-btns">
+                    <Button
+                        onClick={() => this.prevPage()}
+                        variant="raised"
+                        color="primary"
+                        className="prev-btn"
+                        disabled={this.state.currentPage == 1}
+                        >
+                        Prev
+                    </Button>
+                    <Button
+                        onClick={() => this.nextPage()}
+                        variant="raised"
+                        color="primary"
+                        className="next-btn"
+                        disabled={this.state.listings.length === 0}
+                        >
+                        Next
+                    </Button>
+                </div>
             </div>;
 
         return (
